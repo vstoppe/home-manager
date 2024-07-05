03d7daeac455249108cd10dc8dc59f0aa24c2d2d
vstoppe <vst@lindenbox.de>
2024-07-05 23:51:25 +0200

    nvim-tree works

--- home.nix
+++ home.nix
@@ -56,7 +56,7 @@
     shellcheck # neovim / lsp depencency
     unixtools.watch
     #vagrant
-    vimPlugins.nvim-tree-lua
+    # vimPlugins.nvim-tree-lua
     virt-manager
     wakeonlan
     wget
@@ -107,8 +107,8 @@
       nvim-web-devicons # needed for lualine-nvim
       vim-fugitive
       gruvbox-nvim
-      nerdtree
       coc-nvim
+      nvim-tree-lua
     ];
 
 
@@ -116,9 +116,63 @@
       lua << END
         require('lualine').setup{
           options = { theme = 'gruvbox' }
-
         }
+
+        require("nvim-tree").setup({
+          disable_netrw = true,
+          hijack_netrw = true,
+          sort_by = "case_sensitive",
+          view = {
+            width = 30,
+            number = true,
+            relativenumber = true,
+          },
+          renderer = {
+            group_empty = true,
+          },
+          filters = {
+            dotfiles = true,
+          },
+            actions = {
+                open_file = {
+                    quit_on_open = true,
+                },
+            },
+        })
+
+        -- Set leader key to space
+        vim.g.mapleader = ' '
+
+        -- auto close on quitting the last buffer except nvim-tree
+        local function is_modified_buffer_open(buffers)
+            for _, v in pairs(buffers) do
+                if v.name:match("NvimTree_") == nil then
+                    return true
+                end
+            end
+            return false
+        end
+
+        vim.api.nvim_create_autocmd("BufEnter", {
+            nested = true,
+            callback = function()
+                if
+                    #vim.api.nvim_list_wins() == 1
+                    and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil
+                    and is_modified_buffer_open(vim.fn.getbufinfo({ bufmodified = 1 })) == false
+                then
+                    vim.cmd("quit")
+                end
+            end,
+        })
+
+        -- nvim-tree --
+        vim.opt.termguicolors = true
+        local nvimtree = require("nvim-tree.api")
+        vim.keymap.set('n', '<leader>tt', nvimtree.tree.toggle, {})
       END
+
+
 
 
       colorscheme gruvbox
@@ -130,9 +184,6 @@
       " Enable syntax highlighting
       syntax enable
 
-
-      " NERDTree configuration
-      map <C-n> :NERDTreeToggle<CR>
 
       " CoC.nvim configuration
       let g:coc_global_extensions = ['coc-json', 'coc-tsserver', 'coc-python']
