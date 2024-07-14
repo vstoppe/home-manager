6a34902a60e51bfbd41de4002f128e96506f32ae
vstoppe <vst@lindenbox.de>
2024-07-14 22:31:43 +0200

    lsp add additional completion capabilities

--- neovim/completion.nix
+++ neovim/completion.nix
@@ -27,6 +27,9 @@
             return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
         end
 
+        -- add additional completion capabilities:
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
         local luasnip = require("luasnip")
         local cmp = require("cmp")
 
