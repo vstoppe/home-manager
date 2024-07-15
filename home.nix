113fa2c651e7c8a221e48357bfbb3948d96ec31b
vstoppe <vst@lindenbox.de>
2024-07-15 22:01:04 +0200

    zsh config separated

--- home.nix
+++ home.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./neovim.nix
+    ./zsh.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
@@ -99,46 +100,6 @@ programs.powerline-go = {
     settings = { theme = "solarized-dark16"; };
 };
 
-programs.zsh = {
-    enable = true;
-    autocd = true;
-    initExtra = ''
-    compdef kubecolor=kubectl
-    '';
-    # enableAutosuggestions = true; <== outdated with 24.05
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    history.extended = true;
-    historySubstringSearch.enable = true;
-    localVariables = {
-      PATH="$HOME/.nix-profile/bin:/usr/local/bin:$HOME/.krew/bin:$PATH";
-    };
-    shellAliases = {
-      e = "exit";
-      gb = "git branch";
-      gp = "git push";
-      gs = "git status";
-      h3 = "helm";
-      k = "kubectl";
-      kubectl = "kubecolor --light-background";
-      l  = "ls -lh";
-      ll = "ls -lh";
-      nixvim = "nvim";
-      ns  = "switch ns";
-      # nvim = "nvim -u ~/.config/nvim/init.lua";
-      update = "home-manager switch";
-      wp = "watch kubectl get po";
-      ws  = "cd ~/workspace/";
-      ws3 = "cd ~/workspace/k3s";
-      wsa = "cd ~/workspace/ansible";
-      wsh = "cd ~/workspace/homelab";
-    };
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "lxd" ];
-      theme = "agnoster";
-    };
-};
 
   # Let Home Manager install and manage itself.
   programs.git = {
---
+++ zsh.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+    initExtra = ''
+    compdef kubecolor=kubectl
+    '';
+    # enableAutosuggestions = true; <== outdated with 24.05
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    history.extended = true;
+    historySubstringSearch.enable = true;
+    localVariables = {
+      PATH="$HOME/.nix-profile/bin:/usr/local/bin:$HOME/.krew/bin:$PATH";
+    };
+    shellAliases = {
+      e = "exit";
+      gb = "git branch";
+      gp = "git push";
+      gs = "git status";
+      h3 = "helm";
+      k = "kubectl";
+      kubectl = "kubecolor --light-background";
+      l  = "ls -lh";
+      ll = "ls -lh";
+      nixvim = "nvim";
+      ns  = "switch ns";
+      # nvim = "nvim -u ~/.config/nvim/init.lua";
+      update = "home-manager switch";
+      wp = "watch kubectl get po";
+      ws  = "cd ~/workspace/";
+      ws3 = "cd ~/workspace/k3s";
+      wsa = "cd ~/workspace/ansible";
+      wsh = "cd ~/workspace/homelab";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "lxd" ];
+      theme = "agnoster";
+    };
+  };
+}
