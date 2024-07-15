c1574dd9698b0dd570925d47401d3409a0b402a0
vstoppe <vst@lindenbox.de>
2024-07-15 22:10:48 +0200

    last cleanup after refactoring

--- home.nix
+++ home.nix
@@ -21,7 +21,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "23.05"; # Please read the comment before changing.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -59,54 +59,53 @@
     wget
     yq
     zsh
+    # It is sometimes useful to fine-tune packages, for example, by applying
+    # overrides. You can do that directly here, just don't forget the
+    # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+  ];
 
-  # # It is sometimes useful to fine-tune packages, for example, by applying
-  # # overrides. You can do that directly here, just don't forget the
-  # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-  # # fonts?
-  # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-];
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
 
-# Home Manager is pretty good at managing dotfiles. The primary way to manage
-# plain files is through 'home.file'.
-home.file = {
-  # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-  # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-  # # symlink to the Nix store copy.
-  # ".screenrc".source = dotfiles/screenrc;
-
-  # # You can also set the file content immediately.
-  # ".gradle/gradle.properties".text = ''
-  #   org.gradle.console=verbose
-  #   org.gradle.daemon.idletimeout=3600000
-  # '';
-};
-
-# You can also manage environment variables but you will have to manually
-# source
-#
-#  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-home.sessionVariables = {
-  DOCKER_HOST="ssh://root@onyx";
-  EDITOR = "nvim";
-};
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
 
 
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  home.sessionVariables = {
+    DOCKER_HOST="ssh://root@onyx";
+    EDITOR = "nvim";
+  };
 
 
-programs.powerline-go = {
-    enable = true;
-    modules = [ "venv" "user" "host" "cwd" "perms" "git" "exit" "root" "kube" ];
-    settings = { theme = "solarized-dark16"; };
-};
+  programs.powerline-go = {
+      enable = true;
+      modules = [ "venv" "user" "host" "cwd" "perms" "git" "exit" "root" "kube" ];
+      settings = { theme = "solarized-dark16"; };
+  };
 
 
-  # Let Home Manager install and manage itself.
+    # Let Home Manager install and manage itself.
   programs.git = {
     enable    = true;
     #user      = "Volker Stoppe";
     userEmail = "vst@lindenbox.de";
     userName  = "vstoppe";
   };
+
   programs.home-manager.enable = true;
 }
