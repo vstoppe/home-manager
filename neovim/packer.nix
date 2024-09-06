515b6fae338d8c7173bbf96cdad00e1988a12c27
vstoppe <vst@lindenbox.de>
2024-09-07 00:34:18 +0200

    render-markdown works this way

--- neovim/packer.nix
+++ neovim/packer.nix
@@ -32,6 +32,18 @@
         }
       
 
+        -- install markdown support
+        use({
+            'MeanderingProgrammer/render-markdown.nvim',
+            -- after = { 'nvim-treesitter' }, -- dep not found by packer / should be installed by nix
+            requires = { 'echasnovski/mini.nvim', opt = true }, -- if you use the mini.nvim suite
+            -- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
+            requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+            config = function()
+                require('render-markdown').setup({})
+            end,
+        })
+
 
         -- Automatically set up your configuration after cloning packer.nvim
         -- Put this at the end after all plugins
