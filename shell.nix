c8e11bef28223425e433778481290c1d37dfdacf
vstoppe <vst@lindenbox.de>
2024-09-13 20:42:48 +0200

    eza config finished

--- shell.nix
+++ shell.nix
@@ -11,6 +11,16 @@
 
     dircolors.enable = true;
 
+    eza = {
+      enable = true;
+      icons = true;
+      git = true;
+      extraOptions = [
+        "--group-directories-first"
+        "--time-style=+%Y-%m-%d %H:%M"
+      ];
+    };
+
 
     fzf = {
         enable = true;
@@ -28,17 +38,18 @@
         ### initialize zinit plugin manager
         # do "touch $ZINIT[MAN_DIR]/man1/zinit.1" to prevent "/zinit.1: No such file or directory" error
         source "$HOME/.nix-profile/share/zinit/zinit.zsh"
+        zinit snippet OMZP::eza
         zinit snippet OMZP::git
         zinit snippet OMZP::kubectl
         zinit snippet OMZP::kubectx
         
-        compdef kubecolor=kubectl
         source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
         zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color=always $realpath'
       '';
       initExtra = ''
         ### cdreplay should be anabled, throws an error, but nothing is missing
         # zinit cdreplay -q  # Replay compdefs (to be done after compinit)
+        compdef kubecolor=kubectl
       '';
       # enableAutosuggestions = true; <== outdated with 24.05
       autosuggestion.enable = true;
@@ -59,8 +70,8 @@
         h3 = "helm";
         k = "kubectl";
         kubectl = "kubecolor --light-background";
-        l  = "ls -lh";
-        ll = "ls -lh";
+        # l  = "eza -l";
+        # ll = "ls -lh";
         nixvim = "nvim";
         ns  = "switch ns";
         # nvim = "nvim -u ~/.config/nvim/init.lua";
