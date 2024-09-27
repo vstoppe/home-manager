19576253d87e8b56f374ca45539fd5fe6b95b40c
vstoppe <vst@lindenbox.de>
2024-09-27 17:11:16 +0200

    wip lsp-enhancement

--- neovim/lsp-config.nix
+++ neovim/lsp-config.nix
@@ -109,17 +109,24 @@
               schemaStore = {
                 -- You must disable built-in schemaStore support if you want to use
                 -- this plugin and its advanced options like `ignore`.
-                enable = true,
+                enable = false,
                 -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
                 url = "",
               },
               schemas = require('schemastore').yaml.schemas{
+                -- select subset from the JSON schema catalog
                 select = {
                   'kustomization.yaml',
                   'Helm Chart.yaml',
                   'helmfile',
                   'docker-compose.yml',
                 },
+                -- additional schemas (not in the catalog)
+                extra = {
+                  url = 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json',
+                  name = 'Argo CD Application',
+                  fileMatch = 'argocd-application.yaml'
+                }, 
               },
             },
           },
