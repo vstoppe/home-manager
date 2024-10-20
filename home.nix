33d0423cf91cecf3cf9df9bc4ffea75e968bddfb
vstoppe <vst@lindenbox.de>
2024-10-20 14:12:32 +0200

    frappe theme for bat integrated

--- home.nix
+++ home.nix
@@ -121,6 +121,30 @@
       };
     };
   };
+
+  bat = {
+    enable = true;
+    themes = {
+        frappe = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "d2bbee4f7e7d5bac63c054e4d8eca57954b31471";
+            sha256 = "sha256-x1yqPCWuoBSx/cI94eA+AWwhiSA42cLNUOFJl7qjhmw=";
+          };
+          file = "themes/Catppuccin\ Frappe.tmTheme";
+        };
+        dracula = {
+          src = pkgs.fetchFromGitHub {
+            owner = "dracula";
+            repo = "sublime"; # Bat uses sublime syntax for its themes
+            rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
+            sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
+          };
+          file = "Dracula.tmTheme";
+        };
+    };
+  };
     
   home-manager.enable = true;
 
