cb6e660e85ca2bea5143eb1fc8eeb0951733b01f
vstoppe <vst@lindenbox.de>
2025-12-30 07:20:05 +0100

    settings adjustements and added query

--- neovim/treesitter.nix
+++ neovim/treesitter.nix
@@ -3,7 +3,7 @@
 {
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
-      nvim-treesitter
+      nvim-treesitter # should not be install parallel to ..withAllGrammers
       nvim-treesitter.withAllGrammars
       rainbow-delimiters-nvim
     ];
@@ -20,18 +20,14 @@
         end
 
         require'nvim-treesitter.configs'.setup {
-          -- A list of parser names, or "all" (the five listed parsers should always be installed)
-          ensure_installed = { "bash", "dockerfile", "helm", "gotmpl", "html", "json", "json5", "lua", "nix", "markdown", "python", "rust", "sql", "vim", "vimdoc", "yaml" },
+          ensure_installed = { "bash", "dockerfile", "helm", "gotmpl", "html", "json", "json5", "lua", "nix", "markdown", "python", "query", "rust", "sql", "vim", "vimdoc", "yaml" },
 
           -- Install parsers synchronously (only applied to `ensure_installed`)
           sync_install = false,
 
           -- Automatically install missing parsers when entering buffer
           -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-          auto_install = true,
-
-          -- List of parsers to ignore installing (for "all")
-          ignore_install = { "javascript" },
+          auto_install = false,
 
           ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
           -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
@@ -39,12 +35,6 @@
 
           highlight = {
             enable = true,
-
-            -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-            -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-            -- the name of the parser)
-            -- list of language that will be disabled
-            disable = { "c" },
             -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
             disable = function(lang, buf)
                 local max_filesize = 100 * 1024 -- 100 KB
@@ -58,7 +48,7 @@
             -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
             -- Using this option may slow down your editor, and you may see some duplicate highlights.
             -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = true,
+            additional_vim_regex_highlighting = false,
           },
           -- rainbow-delimiters are only activated here:
           rainbow = {
