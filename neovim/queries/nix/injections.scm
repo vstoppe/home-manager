196c334736d5ccae2bc6c43aff73231b2d0ed3ed
vstoppe <vst@lindenbox.de>
2026-01-03 13:05:02 +0100

    added injection for nix/lua

---
+++ neovim/queries/nix/injections.scm
@@ -0,0 +1,34 @@
+;; extends
+; taken from https://github.com/svrana/nix-home.git
+(binding_set
+  (binding
+    attrpath: (attrpath) @_key (#eq? @_key "type")
+    expression: (string_expression) @_value (#eq? @_value "\"lua\""))
+  (binding
+    attrpath: (attrpath) @_keys (#eq? @_keys "config")
+      (indented_string_expression
+        (string_fragment) @injection.content (#set! injection.language "lua")))
+)
+
+
+(binding_set
+  (binding
+    attrpath: (attrpath) @_key (#eq? @_key "type")
+    expression: (string_expression) @_value (#eq? @_value "\"viml\""))
+  (binding
+    attrpath: (attrpath) @_keys (#eq? @_keys "config")
+      (indented_string_expression
+        (string_fragment) @injection.content (#set! injection.language "vim")))
+)
+
+(binding
+	attrpath: (attrpath) @_key (#eq? @_key "extraConfig")
+	(indented_string_expression
+		(string_fragment) @injection.content (#set! injection.language "vim"))
+)
+
+(binding
+	attrpath: (attrpath) @_key (#eq? @_key "extraLuaConfig")
+	(indented_string_expression
+		(string_fragment) @injection.content (#set! injection.language "lua"))
+)
