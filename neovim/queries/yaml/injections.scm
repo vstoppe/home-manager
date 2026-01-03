7ee288462f8e636f762f860902b86be953162a6c
vstoppe <vst@lindenbox.de>
2026-01-03 10:24:02 +0100

    it is working now

--- neovim/queries/yaml/injections.scm
+++ neovim/queries/yaml/injections.scm
@@ -1,16 +1,11 @@
 ; extends
 
-(
-  (block_mapping_pair
-    key: (flow_node
-      (plain_scalar
-        (string_scalar) @key_name))
-    value: (block_node
-      (block_scalar) @injection_content))
-  (#any-of? @key_name "values" "valuesContent")
-  (#offset! @injection_content 1 1 0 0)
-  (#set! injection.language "helm")
-)
+( block_mapping_pair
+    key: (flow_node (plain_scalar) @key)
+    value: (block_node (block_scalar) @injection.content)
+  (#eq? @key "valuesContent")
+  (#offset! @injection.content 0 1 0 0)
+  (#set! injection.language "yaml"))
 
 
 (block_node
@@ -18,4 +13,3 @@
   (#lua-match? @injection.content "^|\n%s*#cloud%-config%s*\n")
   (#offset! @injection.content 0 1 0 0)
   (#set! injection.language "yaml"))
-
